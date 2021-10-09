d1a1641b1e2c757c398cad3379c2ba76a945c854
Sharipov Aidar <sharipov_ai@auca,kg>
2021-10-09 22:01:36 +0200

    lab01/p03 finished

---
+++ lab01/p03/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    const int kNameWidth = 30;
+    const int kAvgWidth = 40;
+    cout << setfill('.') << fixed;
+    for (string name; cin >> name;)
+    {
+        double sum = 0;
+        int nGrades = 0;
+        for (int grade; cin >> grade;)
+        {
+            sum += grade;
+            ++nGrades;
+        }
+        if (nGrades != 0)
+        {
+            cout << left << setw(kNameWidth) << name << "|" << right << setw(kAvgWidth) << setprecision(2) << sum / nGrades << "\n";
+        }
+        else
+        {
+            cout << left << setw(kNameWidth) << name << "|" << right << setw(kAvgWidth + 1) << "no data\n";
+        }
+        cin.clear();
+    }
+}
