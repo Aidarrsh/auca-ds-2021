2001aa1696927e21efad976eeaa25144155e6824
Sharipov Aidar <sharipov_ai@auca,kg>
2021-12-26 15:39:04 +0100

    project 2 problem 14 wrong answer

---
+++ project02/p14/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+    int n;
+    vector<string> v;
+    while (n != 0)
+    {
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            string s;
+            cin >> s;
+            v.emplace_back(s);
+        }
+        stable_sort(begin(v), end(v), [](string a, string b)
+                    {
+                        if (a[0] != b[0])
+                        {
+                            return a < b;
+                        }
+                        else 
+                        {
+                            return a[1] < b[1];
+                        }
+                    });
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << v[i] << "\n";
+        }
+        v.clear();
+        cout <<"\n";
+    }
+}
